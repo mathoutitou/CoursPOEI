f7edc5dbbaf0b699da1ffe0041f8fd9be771986d
Mathilde F <fougeron.mathilde@gmail.com>
2018-11-28 10:21:19 +0100

    avoir un produit courant

--- 05-Angular/angular-shop/src/app/app.component.ts
+++ 05-Angular/angular-shop/src/app/app.component.ts
@@ -8,13 +8,38 @@ import { Plante } from './plante';
 })
 export class AppComponent {
   title = 'Plantastore';
-  plante: Plante ;
+  plantes: Plante[] = []; // [] pour signifier que c'est un tableau
+  currentPlante: Plante; // permet d'avoir un produit courant
 
   constructor() {
-    this.plante = new Plante();
-    this.plante.name = 'Aloe Vera';
-    this.plante.img = '<img src="img/aloe-vera.png" alt="aloe">';
-    this.plante.description = 'LALALALALA Connue pour ses vertus médicinales et cosmétiques.';
-    this.plante.price = 20;
+    this.plantes.push(new Plante());
+    this.plantes[0].name = 'Aloe Vera';
+    this.plantes[0].img = 'aloe-vera.png';
+    this.plantes[0].description = 'Connue pour ses vertus médicinales et cosmétiques.';
+    this.plantes[0].price = 20;
+
+    this.plantes.push(new Plante());
+    this.plantes[1].name = 'Bananier';
+    this.plantes[1].img = 'bananier.png';
+    this.plantes[1].description = 'Dans des conditions idéales, il fera de toutes petites bananes, pas très bonnes.';
+    this.plantes[1].price = 35;
+
+    this.plantes.push(new Plante());
+    this.plantes[2].name = 'Succulente';
+    this.plantes[2].img = 'succulente.png';
+    this.plantes[2].description = 'Très appréciée pour son feuillage en forme de fleur.';
+    this.plantes[2].price = 30;
+
+    this.plantes.push(new Plante());
+    this.plantes[3].name = 'Yucca';
+    this.plantes[3].img = 'yucca.png';
+    this.plantes[3].description = 'Facile d\'entretien, elle est originaire d\'Amérique Centrale.';
+    this.plantes[3].price = 30;
   }
+  // Définir quel est l'objet courant
+  setCurrent(plante: Plante): void {
+    this.currentPlante = plante;
+  }
+
 }
+
